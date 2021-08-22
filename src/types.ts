bc7815a05749ca4f2316626a10b93158473463ac
Michael Blanchard <michael.blanchard@nfl.com>
2021-08-22 12:12:29 -0700

    Moved JS files to TS and TSX

---
+++ src/types.ts
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import {
+  Animated,
+  SpringAnimationConfig,
+  StyleSheet,
+  TimingAnimationConfig,
+} from 'react-native';
+export type ChangeEvent =
+  | 'onSlidingComplete'
+  | 'onSlidingStart'
+  | 'onValueChange';
+export type Dimensions = {
+  height: number;
+  width: number;
+};
+export type SliderProps = {
+  animateTransitions: boolean;
+  animationConfig: {
+    spring?: typeof SpringAnimationConfig;
+    timing?: typeof TimingAnimationConfig;
+  };
+  animationType: 'spring' | 'timing';
+  containerStyle: typeof StyleSheet.styles;
+  debugTouchArea: boolean;
+  disabled: boolean;
+  maximumTrackTintColor: string;
+  maximumValue: number;
+  minimumTrackTintColor: string;
+  minimumValue: number;
+  onSlidingComplete: (value: number | Array<number>) => void;
+  onSlidingStart: (value: number | Array<number>) => void;
+  onValueChange: (value: number | Array<number>) => void;
+  renderAboveThumbComponent?: (index: number) => React.ReactNode;
+  renderThumbComponent: () => React.ReactNode;
+  renderTrackMarkComponent?: (index: number) => React.ReactNode;
+  step: number;
+  thumbImage: string | number | Array<string | number>;
+  thumbStyle: typeof StyleSheet.styles;
+  thumbTintColor: string;
+  thumbTouchSize: Dimensions;
+  trackClickable: boolean;
+  trackMarks?: Array<number>;
+  trackStyle: typeof StyleSheet.styles;
+  value: number | Array<number>;
+};
+export type SliderState = {
+  allMeasured: boolean;
+  containerSize: Dimensions;
+  thumbSize: Dimensions;
+  trackMarksValues?: Array<typeof Animated.Value>;
+  values: Array<typeof Animated.Value>;
+};
