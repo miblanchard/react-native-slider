678b906145aa3afd211a5c5bf48e8a59f0522f2b
Michael Blanchard <michael.blanchard@nfl.com>
2021-08-22 12:09:04 -0700

    Moved JS files to TS and TSX

--- storybook/config.js
+++ storybook/config.js
@@ -1,12 +1,11 @@
-/* @flow */
 import 'resize-observer-polyfill/dist/ResizeObserver.global';
 import {configure} from '@storybook/react-native';
-
 configure(() => {
     const req = require.context(
-        '../src', // path where stories live
-        true, // recursive?
-        /.*__stories__\/.*.js$/, // story files match this pattern
-    );
-    req.keys().forEach((module) => req(module));
+    '../src', // path where stories live
+    true, // recursive?
+    /.*__stories__\/.*.js$/, // story files match this pattern
+  );
+
+  req.keys().forEach((module) => req(module));
 }, module);
